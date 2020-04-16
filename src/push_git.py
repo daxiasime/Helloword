2d1a702a76b39b957fbac973a77405039617865e
daxiaisme <matrix404@163.com>
2020-04-17 07:46:41 +0800

    submit on 2020-04-17 07:46:41

--- src/push_git.py
+++ src/push_git.py
@@ -2,11 +2,9 @@
 import os
 import datetime
 log="submit on "+str(datetime.datetime.now()).split('.')[0]
-_PATH="~/桌面/hello"
 os.chdir('../')
 os.system("git add src ")
 os.system("git add res ")
 os.system(f"git commit -m '{log}'")
 os.system("git push ds master")
-
 print(log)
